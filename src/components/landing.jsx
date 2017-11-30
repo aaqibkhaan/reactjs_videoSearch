19126563823f852f5e37be3bdccd88b6028e402b
aaqibkhaan <aaqibrashid@hotmail.com>
2017-11-30 11:57:14 +0000

    images added

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-const Landing = () => <div> You are on Landing</div>;
+const Landing = () => (
+	<div className="landing">
+		<h1 className="lheading">svideo</h1>
+		<input className="lInput" type="text" placeholder="Search" />
+		<a className="lBrowse">or Browse All</a>
+	</div>
+);
 
 export default Landing;
