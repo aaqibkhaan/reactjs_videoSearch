a29435398f5647e894538fa57ee276100d20c332
aaqibkhaan <aaqibrashid@hotmail.com>
2017-11-30 12:48:32 +0000

    reactjs state added

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
-const Search = () => (
-	<div className="search">
-		<div>
-			{preload.shows.map(show => (
-				<div className="show-card">
-					<img
-						alt={`${show.title} Show Poster`}
-						src={`/public/img/posters/${show.poster}`}
-					/>
-					<div>
-						<h3>{show.title}</h3>
-						<h4>({show.year})</h4>
-						<p>{show.description}</p>
-					</div>
-				</div>
-			))}
-		</div>
-	</div>
-);
+class Search extends Component {
+  state = {
+    searchTerm: ""
+  };
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
