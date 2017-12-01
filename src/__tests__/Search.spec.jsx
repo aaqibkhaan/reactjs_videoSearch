8764a73f9431bef06e873d487b129c5af14b4ff8
aaqibkhaan <aaqibrashid@hotmail.com>
2017-12-01 22:55:05 +0000

    test added for ShowCard.jsx

--- src/__tests__/Search.spec.jsx
+++ src/__tests__/Search.spec.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { shallow } from "enzyme";
+import preload from "../data.json";
 import Search from "../components/search";
+import ShowCard from "../components/ShowCard";
 
 test("Search renders correctly", () => {
 	const component = shallow(<Search />);
 	expect(component).toMatchSnapshot();
+});
+
+test("Search should render correct amount of shows", () => {
+	const component = shallow(
+		<UnwrappedSearch searchTerm="" shows={preload.shows} />
+	);
+	expect(preload.shows.length).toEqual(component.find(ShowCard).length);
 });
