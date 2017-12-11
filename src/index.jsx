5d35f25200cd54bd2ea82e562c4bf7955fa35df1
aaqibkhaan <aaqibrashid@hotmail.com>
2017-12-11 14:24:16 +0000

    hot module replacment added index.js

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,14 @@ import { render } from "react-dom";
 import App from "./components/app";
 /* eslint no-undef: "off" */
 
-const renderApp(){
+const renderApp = () =>{
 	render(<App />, document.getElementById("app"));	
 };
 
 renderApp();
 
 if(module.hot){
-	module.hot.accept('./App', () => {
+	module.hot.accept('./components/app', () => {
 		renderApp();
 	});
 }
