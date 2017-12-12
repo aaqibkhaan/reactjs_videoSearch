7db2d804f9661925c1e4e598f65da69562cf5f22
aaqibkhaan <aaqibrashid@hotmail.com>
2017-12-12 22:00:14 +0000

    Created: header compoenent

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+// @flow
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => (
+	<header>
+		<h1>
+			<Link to="/">Search Videos</Link>
+		</h1>
+	</header>
+);
+export default Header;
